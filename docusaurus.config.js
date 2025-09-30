109ffeff0cb9a9462ca3c87e2df6464f91863d49
Mikers <snissn@gmail.com>
2025-09-30 14:11:19 +0800

    chore(favicon): point favicon to randamu_logo.svg to match brand icon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'OnlySwaps',
   tagline: 'Developer documentation',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/randamu_logo.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
